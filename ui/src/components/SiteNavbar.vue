d9f642e5dc7ab32eb07e0cc19daaf705591e12b4
phakeandy <whota0@163.com>
2025-11-01 19:45:28 +0800

    feat(ui): 新增 selection 效果

--- ui/src/components/SiteNavbar.vue
+++ ui/src/components/SiteNavbar.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="mx-auto flex max-w-2xl justify-between rounded-full border border-gray-300 p-2">
-    <div>
+    <button class="select-none">
       <iconify-icon icon="tabler:cube-3d-sphere" class="mr-1 align-middle text-2xl"></iconify-icon>
       <span class="align-middle font-light">ChainSight</span>
-    </div>
+    </button>
   </nav>
 </template>
