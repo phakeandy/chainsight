6f038a737c8c5c828b5d83690e54c64b8fb81da9
phakeandy <whota0@163.com>
2025-12-03 11:05:16 +0800

    build: 删除 tailwindcss

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import SiteNavbar from './components/SiteNavbar.vue'
-import InputTextArea from './components/InputTextArea.vue'
+// import SiteNavbar from './components/SiteNavbar.vue'
+// import InputTextArea from './components/InputTextArea.vue'
 </script>
 
 <template>
-  <header class="mt-4">
+  <!-- <header class="mt-4">
     <SiteNavbar />
   </header>
   <main>
     <InputTextArea />
-  </main>
+  </main> -->
+  <div></div>
 </template>
 
 <style scoped></style>
