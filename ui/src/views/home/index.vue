60ec43b5bb0cd8d17509459a43c34a8c9aa9479b
phakeandy <whota0@163.com>
2025-12-20 22:19:48 +0800

    wip

--- ui/src/views/home/index.vue
+++ ui/src/views/home/index.vue
@@ -1,9 +1,35 @@
-<template>
-  <h1>hello</h1>
-</template>
-
 <script setup lang="ts">
+import InputArea from './components/InputArea.vue'
 defineOptions({
   name: 'HomePage',
 })
 </script>
+
+<template>
+  <div class="wrapper">
+    <section class="content"></section>
+    <section class="input">
+      <InputArea />
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+
+  display: grid;
+  grid-template:
+    'content' 1fr
+    'input' auto
+    / 1fr;
+
+  & .content {
+    grid-area: content;
+  }
+
+  & .input {
+    grid-area: input;
+  }
+}
+</style>
