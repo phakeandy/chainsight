781bbcee9777220c5f01fd85a8c5b8afcf59db73
phakeandy <whota0@163.com>
2025-12-20 17:13:33 +0800

    新对话

--- ui/src/layouts/two-columns/components/TheSidebar.vue
+++ ui/src/layouts/two-columns/components/TheSidebar.vue
@@ -2,15 +2,24 @@
 <template>
   <div class="sidebar">
     <section>
-      <nav></nav>
+      <nav class="stack box">
+        <button class="btn primary">
+          <span class="with-icon">
+            <iconify-icon icon="tabler:message-circle-plus" class="icon"></iconify-icon>
+            <span>新对话</span>
+          </span>
+        </button>
+      </nav>
     </section>
     <section class="bottom">
-      <div class="stack box">
-        <button class="box with-icon">
-          <iconify-icon icon="tabler:login" class="icon"></iconify-icon>
-          <span> 登陆</span>
+      <nav class="stack box">
+        <button class="btn">
+          <span class="with-icon">
+            <iconify-icon icon="tabler:login" class="icon"></iconify-icon>
+            <span>登陆</span>
+          </span>
         </button>
-      </div>
+      </nav>
     </section>
   </div>
 </template>
